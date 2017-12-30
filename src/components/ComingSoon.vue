1512c99ed4f4330208110ae01b5e68b1f43aa3e4
Riley Dillon <riley@14four.com>
2017-12-29 20:51:13 -0800

    trying a vue build

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="coming-soon">
+    <h1>Coming Soon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.coming-soon {
+  font-size: 50px;
+  top: 50%;
+  transform: translateY(-50%);
+  position: relative;
+  text-align: center;
+}
+h1 {
+  margin: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'vue-event-calendar/dist/style.css' // ^1.1.10, CSS has been extracted as one file, so you can easily update it.
+import vueEventCalendar from 'vue-event-calendar'
+
+Vue.use(vueEventCalendar, {locale: 'en', color: '#2c87aa'}) // locale can be 'zh' , 'en' , 'es', 'pt-br', 'ja', 'ko', 'fr', 'it', 'ru', 'de', 'vi'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
